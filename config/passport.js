0ee42f7a00518ed0a38c216da6ff2607130534d4
agneschang <agneschang0227@gmail.com>
2022-09-21 23:12:02 +0800

    profile:session&cookie => serialize/deserialize User

--- config/passport.js
+++ config/passport.js
@@ -2,33 +2,41 @@ import passport from "passport";
 import GoogleStrategy from "passport-google-oauth20";
 import User from "../models/user-model.js";
 
-passport.use(
-    new GoogleStrategy(
-        {//檢查
-            clientID: "746570348159-amsb83ddgqq4sh57cracdfs7iom8730m.apps.googleusercontent.com",
-            clientSecret: "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",
-            //將這個url填寫到創建用戶端ID的重新導向URI
-            callbackURL: "/auth/google/redirect",
-        },//成功則進行以下callback fn
-        (accessToken, refreshToken, profile, done) => {//passport callback
-            console.log(profile);
-            User.findOne({googleID: profile.id})
-                .then((foundUser) => {
-                    if (foundUser) {
-                        console.log("User already exist");
-                        done(null, foundUser);
-                    } else {
-                        new User({
-                            name: profile.displayName,
-                            googleID: profile.id,
-                            thumbnail: profile.photos[0].value,
-                        }).save().then((newUser) => {
-                            console.log("new user created");
-                            done(null, newUser);
-                        })
-                    }
-                })
-        }
-    ))
+//創建一個cookie => serialize UserID
+passport.serializeUser((user, done) => {
+    console.log("Serializing user now");
+    done(null, user._id);
+});
+
+passport.deserializeUser((_id,done)=>{
+    console.log("Deserializing user now ");
+    User.findById({_id}).then((user)=>{
+        console.log("found user");
+        done(null,user);
+    })
+})
+
+passport.use(new GoogleStrategy({//檢查
+        clientID: "746570348159-amsb83ddgqq4sh57cracdfs7iom8730m.apps.googleusercontent.com",
+        clientSecret: "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", //將這個url填寫到創建用戶端ID的重新導向URI
+        callbackURL: "/auth/google/redirect",
+    },//成功則進行以下callback fn
+    (accessToken, refreshToken, profile, done) => {//passport callback
+        console.log(profile);
+        User.findOne({googleID: profile.id})
+            .then((foundUser) => {
+                if (foundUser) {
+                    console.log("User already exist");
+                    done(null, foundUser);
+                } else {
+                    new User({
+                        name: profile.displayName, googleID: profile.id, thumbnail: profile.photos[0].value,
+                    }).save().then((newUser) => {
+                        console.log("new user created");
+                        done(null, newUser);
+                    })
+                }
+            })
+    }))
 
 
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import mongoose from "mongoose";
 import authRoute from "./routes/auth-route.js";
 import profileRoute from "./routes/profileRoute.js";
 import "./config/passport.js";
+import cookieSession from "cookie-session";
+import passport from "passport";
 
 mongoose.connect("mongodb://localhost:27017/myDatabase",{
     useNewUrlParser:true,
@@ -20,12 +22,17 @@ mongoose.connect("mongodb://localhost:27017/myDatabase",{
 app.set("view engine","ejs");
 app.use(express.json());//express 裡面包括了body-parser
 app.use(express.urlencoded({extended:true}));
-app.use("/auth",authRoute);//get"/auth" 就進入authRoute
-app.use("/profile",profileRoute);
+app.use(cookieSession({
+    keys:[process.env.SECRET],
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/",(req,res)=>{
     res.render("index");
 })
+app.use("/auth",authRoute);//get"/auth" 就進入authRoute
+app.use("/profile",profileRoute);
 
 app.listen(5080,()=>{
     console.log("Server running on port 5080");
